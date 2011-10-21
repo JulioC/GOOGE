ce5019f30987266eec1f82cb92b519df54f0f1f1
Júlio Souza <mail@thejcs.com.br>
2011-10-21 20:10:08 -0200

    implementing Vector methods

--- includes/Vector.h
+++ includes/Vector.h
@@ -3,23 +3,40 @@
 
 class Vector {
 public:
-    Vector(float x, float y): 
-    _x(x),
-    _y(y) {
-        
-    };
-    ~Vector() {
+    Vector(float x, float y);
+    Vector(const Vector& orig);    
+    ~Vector();
+    
+    const Vector& operator=(const Vector& rh);
 
-    }
+    Vector operator+(const Vector& rh) const;
+    const Vector& operator+=(const Vector& rh);
+    
+    Vector operator-(const Vector& rh) const;
+    const Vector& operator-=(const Vector& rh);
+    
+    Vector operator*(const float& rh) const;
+    const Vector& operator*=(const float& rh);
+    
+    Vector operator/(const float& rh) const;
+    const Vector& operator/=(const float& rh);
+    
+    bool operator==(const Vector& rh) const;
+    
+    float dotProduct(const Vector& rh);
+    
+    void normalize();
+    float magnitude();
+    
+    float x() const;
+    float y() const;
     
     // @TODO: Implement vector operations 
-
-    float _x, _y;
     
 private:
-    Vector(const Vector&);
-    const Vector& operator=(const Vector&);
-
+    float _x;
+    float _y;
+    
 };
 
 #endif	/* VECTOR_H */
--- sources/Vector.cpp
+++ sources/Vector.cpp
@@ -1,3 +1,82 @@
 #include "Vector.h"
 
-// @TODO: Implement vector operations
+Vector::Vector(float x, float y):
+_x(x),
+_y(y) {
+}
+
+Vector::Vector(const Vector& orig):
+_x(orig._x),
+_y(orig._y) {
+}
+
+const Vector& Vector::operator =(const Vector& rh) {
+    _x = rh._x;
+    _y = rh._y;
+    return *this;
+}
+
+Vector Vector::operator +(const Vector& rh) const {
+    float x = _x + rh._x;
+    float y = _y + rh._y;
+    return Vector(x, y);
+}
+
+const Vector& Vector::operator +=(const Vector& rh) {
+    _x += rh._x;
+    _y += rh._y;
+    return *this;
+}
+
+Vector Vector::operator -(const Vector& rh) const {
+    float x = _x - rh._x;
+    float y = _y - rh._y;
+    return Vector(x, y);
+}
+
+const Vector& Vector::operator -=(const Vector& rh) {
+    _x -= rh._x;
+    _y -= rh._y;
+    return *this;
+}
+
+Vector Vector::operator*(const float& rh) const {
+    float x = _x * rh;
+    float y = _y * rh;    
+    return Vector(x, y);
+}
+
+const Vector& Vector::operator *=(const float& rh) {
+    _x *= rh;
+    _y *= rh;
+    return *this;
+}
+
+Vector Vector::operator /(const float& rh) const {
+    float x = _x / rh;
+    float y = _y / rh;
+    return Vector(x, y);
+}
+
+const Vector& Vector::operator /=(const float& rh) {
+    _x /= rh;
+    _y /= rh;
+    return *this;
+}
+
+bool Vector::operator ==(const Vector& rh) const {
+    return (_x == rh._x && _y == rh._y);
+}
+
+float Vector::dotProduct(const Vector& rh) {
+    return (_x*rh._x + _y*rh._y);
+}
+
+float Vector::x() const {
+    return _x;
+}
+
+float Vector::y() const {
+    return _y;
+}
+
