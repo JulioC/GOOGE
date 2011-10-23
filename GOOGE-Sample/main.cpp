21f905c2e75b250b23b46ba47c1c2cc2ec663fbf
Murilo Fioravanti <lilofc@hotmail.com>
2011-10-23 19:49:43 -0200

    created the Sample project

---
+++ GOOGE-Sample/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: murilo
+ *
+ * Created on October 23, 2011, 7:47 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
