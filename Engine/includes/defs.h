beb8ab9f013b4ee8c336d94ca31173a488b0ca84
julio <julio@localhost.localdomain>
2011-11-13 20:03:54 -0200

    better  exception handling

--- Engine/includes/defs.h
+++ Engine/includes/defs.h
@@ -10,7 +10,37 @@
 
 typedef SDL_Color Color;
 
-class load_error { };
+class Exception {
+public:
+    Exception(const char* message) {
+        _message = new char[sizeof(message)];
+        strcpy(_message, message);
+    }
+    virtual ~Exception() {
+        delete[] _message;
+    }
+    
+    const char* message() {
+        return _message;
+    }
+    
+private:
+    char* _message;
+};
+
+class InitException : public Exception {
+public:
+    InitException(const char* message):
+    Exception(message) {        
+    }
+};
+
+class LoadException : public Exception {
+public:
+    LoadException(const char* message):
+    Exception(message) {        
+    }
+};
 
 #endif	/* DEFS_H */
 
